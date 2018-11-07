50c5a17a24990f6e09fafd002fd39dd4ba0f0499
Jacob Deppen <deppen.8@gmail.com>
2018-11-07 11:36:21 -0800

    group survey_sim files

---
+++ code/survey_sim/survey_unit.py
@@ -0,0 +1,19 @@
+"""
+Description of file
+"""
+
+
+class SurveyStrategy(object):
+    """A set of shapes to use for the survey
+
+    """
+    def __init__(self):
+        self.layers = {}
+
+    def add_search_points(self):
+        pass
+
+    def add_checkerboard(self):
+        pass
+
+
