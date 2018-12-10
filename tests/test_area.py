5b6a243c2ae72d8cb8aacaa78a5528ce5ea014ba
Jacob Deppen <deppen.8@gmail.com>
2018-12-10 00:31:04 -0800

    add first tests for Area

---
+++ tests/test_area.py
@@ -0,0 +1,21 @@
+from surveysim import Area
+from geopandas import GeoDataFrame
+
+class TestArea(object):
+
+    def setup_method(self):
+        self.default = Area()
+    
+    def test_default_is_gdf(self):
+        assert isinstance(self.default.data, GeoDataFrame)
+
+    def test_default_area_measures(self):
+        assert self.default.data.bounds.minx == 0
+        assert self.default.data.bounds.miny == 0
+        assert self.default.data.bounds.maxx == 1
+        assert self.default.data.bounds.maxy == 1
+        assert self.default.data.area == 1
+
+    def test_default_columns_exist(self):
+        for col in ['area_name', 'visibility', 'geometry']:
+            assert col in self.default.data.columns
