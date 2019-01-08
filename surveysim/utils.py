66f963effe3c5140b7b365a9e8119cb551cf5438
Jacob Deppen <deppen.8@gmail.com>
2019-01-08 15:05:22 -0800

    add clip functions for gdf

---
+++ surveysim/utils.py
@@ -0,0 +1,66 @@
+"""General utility functions
+"""
+
+import geopandas as gpd
+
+def clip_points(points: gpd.GeoDataFrame, by: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+    """Subset a GeoDataFrame of points based on the boundaries of another GeoDataFrame
+    
+    Parameters
+    ----------
+    points : geopandas `GeoDataFrame`
+        Point features to be clipped
+    by : geopandas `GeoDataFrame`
+        Boundaries to use for clipping
+    
+    Returns
+    -------
+    geopandas `GeoDataFrame`
+        A subset of the original `points`
+
+    References
+    ----------
+    Earth Analytics Python course, https://doi.org/10.5281/zenodo.2209415
+    """
+
+    poly = by.geometry.unary_union
+    return points[points.geometry.intersects(poly)]
+
+
+def clip_lines_polys(lines_polys: gpd.GeoDataFrame, by: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+    """Subset a GeoDataFrame of lines or polygons based on the boundaries of another GeoDataFrame
+    
+    Parameters
+    ----------
+    lines_polys : geopandas `GeoDataFrame`
+        Features to be clipped
+    by : geopandas `GeoDataFrame`
+        Boundaries to use for clipping
+    
+    Returns
+    -------
+    geopandas `GeoDataFrame`
+        A subset of the original `lines_polys`
+
+    References
+    ----------
+    Earth Analytics Python course, https://doi.org/10.5281/zenodo.2209415
+    """
+    
+    # Create a single polygon object for clipping
+    poly = by.geometry.unary_union
+    spatial_index = lines_polys.sindex  
+
+    # Create a box for the initial intersection
+    bbox = poly.bounds
+
+    # Get a list of id's for each road line that overlaps the bounding box and subset the data to just those lines
+    sidx = list(spatial_index.intersection(bbox))
+    thing_sub = lines_polys.iloc[sidx]
+
+    # Clip the data - with these data
+    clipped = thing_sub.copy()
+    clipped['geometry'] = thing_sub.intersection(poly)
+
+    # Return the clipped layer with no null geometry values
+    return clipped[clipped.geometry.notnull()]
