bf94c74be7ca0200deb9cfb214678608f34e3088
Jacob Deppen <deppen.8@gmail.com>
2019-01-08 21:51:26 -0800

    minor change to TODOs

--- surveysim/area.py
+++ surveysim/area.py
@@ -1,7 +1,7 @@
 """
 Create and modify Area objects
 """
-# TODO: clean up with @classmethod, @staticmethod
+# TODO: clean up with classmethod, staticmethod
 # TODO: document functions
 # TODO: coordinate systems and projections...UGGGGGHHH
 
