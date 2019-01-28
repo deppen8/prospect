35e9639b825237f670d21125339dbcea94b0fcf6
Jacob Deppen <deppen.8@gmail.com>
2019-01-28 13:33:45 -0800

    create Area fixtures for from_shapefile and from_shapely_polygon methods

---
+++ tests/conftest.py
@@ -0,0 +1,57 @@
+import pytest
+import surveysim
+from pathlib import Path
+from shapely.geometry import Polygon
+
+# define `Area` fixtures
+
+
+@pytest.fixture()
+def default_Area():
+    """Create Area with defaults"""
+    return surveysim.Area()
+
+
+# FIXTURES FOR THE `Area.from_shapefile()` METHOD
+
+
+leiap_area_paths = ['leiap_field1.shp', 'leiap_field2.shp', 'leiap_field3.shp', 'leiap_field4.shp',
+                    'leiap_field5.shp', 'leiap_field6.shp', 'leiap_field7.shp', 'leiap_field8.shp']
+
+
+@pytest.fixture(params=leiap_area_paths, ids=leiap_area_paths, scope='session')
+def an_area_shapefile_path(request):
+    return Path(f'tests/test_datasets/shapefiles/{request.param}')
+
+
+@pytest.fixture(params=leiap_area_paths, ids=leiap_area_paths, scope='session')
+def an_area_from_shapefile(request):
+    area = surveysim.Area.from_shapefile(name='test_area_from_shapefile', path=Path(
+        f'tests/test_datasets/shapefiles/{request.param}'))
+    return area
+
+
+# FIXTURES FOR THE `Area.from_shapely_polygon()` METHOD
+
+
+shapely_polys = [
+    Polygon([(1, 1), (1, 3), (4, 3), (4, 1), (1, 1)]),
+    Polygon([(0, 0), (1, 1), (1, 0)]),
+    Polygon([(0.0, 0.0), (0.0, 2.0), (2.0, 2.0), (2.0, 0.0)]),
+    Polygon([(0.0, 0.0), (0.0, 1.0), (1.0, 1.0), (2.0, -1.0), (0.0, 0.0),
+             (0.25, 0.25), (0.25, 0.5), (0.5, 0.5), (0.5, 0.25), (0.25, 0.25)])
+]
+
+
+@pytest.fixture(params=shapely_polys, scope='session')
+def a_shapely_polygon(request):
+    return request.param
+
+
+@pytest.fixture(params=shapely_polys, scope='session')
+def an_area_from_shapely_polygon(request):
+    area = surveysim.Area.from_shapely_polygon(name='test_area_from_shapely', polygon=request.param)
+    return area
+
+
+# define `Layer` fixtures
