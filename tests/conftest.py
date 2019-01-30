19f7b3258b9e645dadb57ded7e435b65f0a72b1d
Jacob Deppen <deppen.8@gmail.com>
2019-01-30 10:15:02 -0800

    Improve Areas for Layer fixtures; Create Assemblage fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,26 +5,27 @@ from pathlib import Path
 
 # `Area` FIXTURES FOR USE IN OTHER MODULES
 
-leiap_area_paths = ['leiap_field1.shp', 'leiap_field2.shp', 'leiap_field3.shp', 'leiap_field4.shp',
+
+LEIAP_AREA_PATHS = ['leiap_field1.shp', 'leiap_field2.shp', 'leiap_field3.shp', 'leiap_field4.shp',
                     'leiap_field5.shp', 'leiap_field6.shp', 'leiap_field7.shp', 'leiap_field8.shp']
 
 
-@pytest.fixture(params=leiap_area_paths, ids=leiap_area_paths, scope='session')
+@pytest.fixture(params=LEIAP_AREA_PATHS, ids=LEIAP_AREA_PATHS, scope='session')
 def an_area_shapefile_path(request):
     return Path(f'test_datasets/shapefiles/areas/{request.param}')
 
 
-@pytest.fixture(params=leiap_area_paths, ids=leiap_area_paths, scope='session')
+@pytest.fixture(params=LEIAP_AREA_PATHS, ids=LEIAP_AREA_PATHS, scope='session')
 def an_area_from_shapefile(request):
     area = surveysim.Area.from_shapefile(name='test_area_from_shapefile', path=Path(
         f'test_datasets/shapefiles/areas/{request.param}'))
     return area
 
 
-# FIXTURES FOR `Layer`
+# `Layer` FIXTURES FOR USE IN OTHER MODULES
 
 
-leiap_layer_paths = [
+LEIAP_LAYER_PATHS = [
     'leiap_field1_points.shp',
     'leiap_field2_points.shp',
     'leiap_field3_points.shp',
@@ -35,16 +36,38 @@ leiap_layer_paths = [
     'leiap_field8_polygon_single.shp'
 ]
 
+area_layer_tuples = list(zip(LEIAP_AREA_PATHS, LEIAP_LAYER_PATHS))
 
-@pytest.fixture(params=leiap_layer_paths, ids=leiap_layer_paths, scope='session')
+
+@pytest.fixture(params=LEIAP_LAYER_PATHS, ids=LEIAP_LAYER_PATHS, scope='session')
 def a_layer_shapefile_path(request):
     return Path(f'test_datasets/shapefiles/layers/{request.param}')
 
 
-@pytest.fixture(params=leiap_layer_paths, ids=leiap_layer_paths, scope='session')
+@pytest.fixture(params=area_layer_tuples, ids=area_layer_tuples, scope='session')
 def a_layer_from_shapefile(request):
-    default_Area = surveysim.Area.from_area_value(
-        name='test_area4layer', value=1.0)
+    area = surveysim.Area.from_shapefile(name='test_area4layer', path=Path(
+        f'test_datasets/shapefiles/areas/{request.param[0]}'))
     layer = surveysim.Layer.from_shapefile(path=Path(
-        f'test_datasets/shapefiles/layers/{request.param}'), area=default_Area, name='test_layer', feature_type=None)
+        f'test_datasets/shapefiles/layers/{request.param[1]}'), area=area, name='test_layer', feature_type=None)
     return layer
+
+
+@pytest.fixture(params=[area_layer_tuples], scope='session')
+def a_layer_list(request):
+    layer_list = []
+    for a, l in request.param:
+        area = surveysim.Area.from_shapefile(name='test_area4layer', path=Path(
+            f'test_datasets/shapefiles/areas/{a}'))
+        layer = surveysim.Layer.from_shapefile(path=Path(
+            f'test_datasets/shapefiles/layers/{l}'), area=area, name='test_layer', feature_type=None)
+        layer_list.append(layer)
+    return layer_list
+
+
+# `Assemblage` FIXTURES FOR USE IN OTHER MODULES
+
+
+@pytest.fixture(scope='session')
+def an_assemblage(a_layer_list):
+    return surveysim.Assemblage(name='test_assemblage', layers=a_layer_list)
