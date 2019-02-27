52c97d2c8bf0baf9177b1975ab824b3ca7d37d67
Jacob Deppen <deppen.8@gmail.com>
2019-02-27 14:34:03 -0800

    fix test data file paths

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,13 +12,13 @@ LEIAP_AREA_PATHS = ['leiap_field1.shp', 'leiap_field2.shp', 'leiap_field3.shp', 
 
 @pytest.fixture(params=LEIAP_AREA_PATHS, ids=LEIAP_AREA_PATHS, scope='session')
 def an_area_shapefile_path(request):
-    return Path(f'test_datasets/shapefiles/areas/{request.param}')
+    return Path(f'tests/test_datasets/shapefiles/areas/{request.param}')
 
 
 @pytest.fixture(params=LEIAP_AREA_PATHS, ids=LEIAP_AREA_PATHS, scope='session')
 def an_area_from_shapefile(request):
     area = surveysim.Area.from_shapefile(name='test_area_from_shapefile', path=Path(
-        f'test_datasets/shapefiles/areas/{request.param}'))
+        f'tests/test_datasets/shapefiles/areas/{request.param}'))
     return area
 
 
@@ -41,15 +41,15 @@ area_layer_tuples = list(zip(LEIAP_AREA_PATHS, LEIAP_LAYER_PATHS))
 
 @pytest.fixture(params=LEIAP_LAYER_PATHS, ids=LEIAP_LAYER_PATHS, scope='session')
 def a_layer_shapefile_path(request):
-    return Path(f'test_datasets/shapefiles/layers/{request.param}')
+    return Path(f'tests/test_datasets/shapefiles/layers/{request.param}')
 
 
 @pytest.fixture(params=area_layer_tuples, scope='session')
 def a_layer_from_shapefile(request):
     area = surveysim.Area.from_shapefile(name='test_area4layer', path=Path(
-        f'test_datasets/shapefiles/areas/{request.param[0]}'))
+        f'tests/test_datasets/shapefiles/areas/{request.param[0]}'))
     layer = surveysim.Layer.from_shapefile(path=Path(
-        f'test_datasets/shapefiles/layers/{request.param[1]}'), area=area, name='test_layer', feature_type=None)
+        f'tests/test_datasets/shapefiles/layers/{request.param[1]}'), area=area, name='test_layer', feature_type=None)
     return layer
 
 
@@ -58,9 +58,9 @@ def a_layer_list(request):
     layer_list = []
     for a, l in request.param:
         area = surveysim.Area.from_shapefile(name='test_area4layer', path=Path(
-            f'test_datasets/shapefiles/areas/{a}'))
+            f'tests/test_datasets/shapefiles/areas/{a}'))
         layer = surveysim.Layer.from_shapefile(path=Path(
-            f'test_datasets/shapefiles/layers/{l}'), area=area, name='test_layer', feature_type=None)
+            f'tests/test_datasets/shapefiles/layers/{l}'), area=area, name='test_layer', feature_type=None)
         layer_list.append(layer)
     return layer_list
 
