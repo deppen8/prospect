754695fcb62ed242829ce49db6595701df484fb3
Jacob Deppen <deppen.8@gmail.com>
2019-03-13 23:08:10 -0700

    add docstrings to simulation

--- surveysim/simulation.py
+++ surveysim/simulation.py
@@ -1,7 +1,3 @@
-"""Handle creation of a simulation session
-
-"""
-
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
@@ -11,6 +7,14 @@ Base = declarative_base()
 
 
 class SimSession():
+    """Object to handle setup/teardown as well as transactions for a SQLAlchemy `Session`
+
+    Attributes
+    ----------
+    session : SQLAlchemy `Session`
+
+    """
+
     from .survey import Survey
     from .area import Area
     from .assemblage import Assemblage
@@ -22,6 +26,19 @@ class SimSession():
     from .surveyor import Surveyor
 
     def __init__(self, engine_str="sqlite:///simulation_default.db"):
+        """Begin a SQLAlchemy `Session`
+
+        Parameters
+        ----------
+        engine_str : str, optional
+            Database URL (the default is "sqlite:///simulation_default.db")
+
+        Notes
+        -----
+        The SQLAlchemy docs have some good examples of different types of database URLs. `https://docs.sqlalchemy.org/en/latest/core/engines.html#database-urls <https://docs.sqlalchemy.org/en/latest/core/engines.html#database-urls>`_
+
+        """
+
         engine = create_engine(engine_str)
         Base.metadata.create_all(engine)
         Session = sessionmaker(bind=engine)
@@ -29,15 +46,20 @@ class SimSession():
 
     def stage(self, block):
         """Add a building block to the list of objects to save to the database
+
         """
+
         self.session.add(block)
 
     def unstage(self, block):
         """Remove a building block
+
         """
+
         self.session.expunge(block)
 
     def remove_from_db(self, block):
+
         self.session.delete(block)
 
 
