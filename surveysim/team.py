542e837b03b5428fb48d6bcdb3403ab790b51176
Jacob Deppen <deppen.8@gmail.com>
2019-03-13 22:03:10 -0700

    full Team integration with sqlalchemy

--- surveysim/team.py
+++ surveysim/team.py
@@ -1,8 +1,13 @@
 
 from .simulation import Base
+from .surveyor import Surveyor
 
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, PickleType, ForeignKey
 from sqlalchemy.orm import relationship
+
+from typing import List
+
+import pandas as pd
 
 
 class Team(Base):
@@ -10,9 +15,17 @@ class Team(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column('name', String(50), unique=True)
+    survey_name = Column('survey_name', String(50), ForeignKey('surveys.name'))
+    surveyor_list = Column('surveyor_list', PickleType)
 
     # relationships
-    survey_name = Column('survey_name', String(50), ForeignKey('surveys.name'))
     survey = relationship("Survey", back_populates='team')
-
     surveyors = relationship("Surveyor", back_populates='team')
+
+    def __init__(self, name: str, survey_name: str, surveyor_list: List[Surveyor]):
+        self.name = name
+        self.survey_name = survey_name
+        self.surveyor_list = surveyor_list
+
+        self.df = pd.DataFrame([surveyor.to_dict()
+                                for surveyor in self.surveyor_list])
