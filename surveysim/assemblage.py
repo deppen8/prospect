b8b91d89e6dd7540a397e9ae15c81974c6b48371
Jacob Deppen <deppen.8@gmail.com>
2019-03-14 09:56:50 -0700

    add docstrings to Assemblage class

--- surveysim/assemblage.py
+++ surveysim/assemblage.py
@@ -1,21 +1,42 @@
-"""A collection of `Layer` objects
-
-"""
-
 
 from .simulation import Base
 from .layer import Layer
 
+from typing import List
+
 from sqlalchemy import Column, Integer, String, PickleType, ForeignKey
 from sqlalchemy.orm import relationship
-
-from typing import List
 
 from geopandas import GeoDataFrame
 import pandas as pd
 
 
 class Assemblage(Base):
+    """A collection of all `Layer` objects for a survey
+    
+    Parameters
+    ----------
+    name : str
+        Unique name for the assemblage
+    survey_name : str
+        Name of the survey
+    area_name : str
+        Name of the containing area
+    layer_list : List[Layer]
+        List of layers that make up the assemblage
+
+    Attributes
+    ----------
+    name : str
+        Name of the assemblage
+    survey_name : str
+        Name of the survey
+    area_name : str
+        Name of the containing area
+    df : geopandas `GeoDataFrame`
+        `GeoDataFrame` with a row for each feature in the assemblage
+    """
+
     __tablename__ = 'assemblages'
 
     id = Column(Integer, primary_key=True)
@@ -31,6 +52,7 @@ class Assemblage(Base):
     # features = relationship("Feature", back_populates='assemblage')
 
     def __init__(self, name: str, survey_name: str, area_name: str, layer_list: List[Layer]):
+
         self.name = name
         self.survey_name = survey_name
         self.area_name = area_name
