e7a377a7d1c9bb08e838151e292755f852fc7185
Jacob Deppen <deppen.8@gmail.com>
2019-03-14 23:39:21 -0700

    initial commit of test_simulation.py

---
+++ tests/test_simulation.py
@@ -0,0 +1,14 @@
+
+import surveysim
+
+
+def test_returns_SimSession(a_simulation):
+    assert isinstance(a_simulation, surveysim.SimSession)
+
+
+def test_has_engine_attribute(a_simulation):
+    assert hasattr(a_simulation, 'engine')
+
+
+def test_has_session_attribute(a_simulation):
+    assert hasattr(a_simulation, 'session')
