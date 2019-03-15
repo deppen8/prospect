b640d8279c6ae0aa9698080aaacac22d12f38da7
Jacob Deppen <deppen.8@gmail.com>
2019-03-14 20:50:31 -0700

    remove pre-sqlalchemy code

--- surveysim_nodb/__init__.py
+++
@@ -1,6 +0,0 @@
-from .area import Area
-from .assemblage import Assemblage
-from .layer import Layer
-from .coverage import Coverage
-from .team import Team
-from .surveyor import Surveyor
--- surveysim_nodb/simulation.py
+++
@@ -1,9 +0,0 @@
-"""[summary]
-Steps
-- find artifacts that lie within survey units
-- find distance from artifacts to surveyor (center of survey unit)
-- apply distance decay function to get probability
-- 
-
-"""
-
